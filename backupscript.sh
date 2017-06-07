4f2db1dba9933d51862dd4d8975d697481e156e8
sanktm <san@yahoo.com>
2017-06-07 10:30:38 +0000

    ii

--- backupscript.sh
+++ backupscript.sh
@@ -12,11 +12,11 @@ fi
 # loop over the array of directories and back them up
 for i in ${arr[@]}; do
     # clean up the repo - remove dangling objects, compress loose objects
-    find $i -name '*.git' -execdir sh -c 'cd {} && git gc' \;
+    find $i -name '*.*' -execdir sh -c 'cd {} && git gc' \;
     # clone the repository bare, no need for working tree
     git clone $i /tmp/$i
     # transfer the repository to backup.com for backup
-    rsync -av --delete /tmp/$i /path/to/backup/location/$i
+    rsync -av /tmp/$i /path/to/backup/location/$i
     # remove our bare copy of the repository
     rm -rf /tmp/$i
 done
