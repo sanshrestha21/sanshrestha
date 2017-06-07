16581e1b22cc4954e412a9886b2f542d8afd1a8f
sanktm <san@yahoo.com>
2017-06-07 10:26:46 +0000

    add

--- backupscript.sh
+++ backupscript.sh
@@ -3,7 +3,7 @@
 # if no argument is given, list all directories and back them up
 if [[ -z $1 ]]; then
     # create array of all directories excluding projectXYZ
-    declare -a arr=(`ls -F . | egrep -v '/home/jack'`)
+    declare -a arr=(`ls -F . | egrep -v 'jack/'`)
 else
     # else we'll only backup the given argument $1
     declare -a arr=(`ls -F . | grep $1`)
@@ -12,12 +12,11 @@ fi
 # loop over the array of directories and back them up
 for i in ${arr[@]}; do
     # clean up the repo - remove dangling objects, compress loose objects
-    find $i -name *.* -execdir sh -c cd {} && git gc \;
+    find $i -name '*.git' -execdir sh -c 'cd {} && git gc' \;
     # clone the repository bare, no need for working tree
     git clone $i /tmp/$i
-    #cp /tmp/$i /path/to/backup/location/$i
     # transfer the repository to backup.com for backup
-    rsync -av  /tmp/$i /path/to/backup/location/$i
+    rsync -av --delete /tmp/$i /path/to/backup/location/$i
     # remove our bare copy of the repository
     rm -rf /tmp/$i
 done
